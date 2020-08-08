2c4bdf2f525eb235806adfeed33525c3b3159add
fersorc <fersorc12@gmail.com>
2020-08-08 00:51:05 -0400

    Adding first-listeners

---
+++ js/listener.js
@@ -0,0 +1,18 @@
+const card = document.querySelector('.card');
+
+
+
+
+
+
+const cardAction = () => {
+   if(card.classList.contains('is-active')) {
+      card.classList.remove('is-active');
+   } else {
+      card.classList.add('is-active');
+   }
+}
+
+
+
+card.addEventListener('click', cardAction)
